7480edeb4c146493c513ab56d48ae3741e360002
Kevin <buddhimahewa@gmail.com>
2022-02-23 19:55:16 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/pethub/MainActivity.kt
+++ app/src/main/java/com/example/pethub/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.pethub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         startActivity(Intent(this, Notification::class.java))
+
+        // connect to firebase db
+        var database = FirebaseDatabase.getInstance().reference
     }
 }

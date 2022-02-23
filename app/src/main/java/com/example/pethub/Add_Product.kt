681449ebed2af0b92a01cb4090696074718fbe8f
Lahiru-cyber <64624843+Lahiru-cyber@users.noreply.github.com>
2022-02-23 17:42:33 +0530

    updated

---
+++ app/src/main/java/com/example/pethub/Add_Product.kt
@@ -0,0 +1,11 @@
+package com.example.pethub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Add_Product : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_product)
+    }
+}

a0d3d07271a33a715485b396b67544e6b571b7ea
Kevin <buddhimahewa@gmail.com>
2022-02-28 04:49:43 +0530

    Doctor and Services - 2

--- app/src/main/java/com/example/pethub/UpdateVet.kt
+++ app/src/main/java/com/example/pethub/UpdateVet.kt
@@ -3,10 +3,9 @@ package com.example.pethub
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AddDoctor : AppCompatActivity() {
-
+class UpdateVet : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_doctor)
+        setContentView(R.layout.activity_update_vet)
     }
 }

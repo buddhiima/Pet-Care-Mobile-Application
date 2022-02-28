94ab09c9c33a527f7060f0cf649999b6fc396584
Lahiru-cyber <64624843+Lahiru-cyber@users.noreply.github.com>
2022-02-28 22:53:29 +0530

    updated add product(insert), Notification, Active Listing, myProfile

--- app/src/main/java/com/example/pethub/Add_Product.kt
+++ app/src/main/java/com/example/pethub/Add_Product.kt
@@ -3,11 +3,26 @@ package com.example.pethub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
-import com.google.firebase.database.DatabaseReference
+import android.widget.Toast
+import com.example.pethub.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+//import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class Add_Product : AppCompatActivity() {
-    
+    private var database = FirebaseDatabase.getInstance("https://my-pethub-default-rtdb.firebaseio.com/")
+    private var databaseref = database.reference.child("Prodcuts")
+
+    companion object{
+        var getproduct : String= String()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
@@ -26,5 +41,42 @@ class Add_Product : AppCompatActivity() {
         home.setOnClickListener{
             startActivity(Intent(this@Add_Product, MainActivity::class.java))
         }
+
+   /*     var productname = findViewById<EditText>(R.id.productname)
+        var productprice = findViewById<EditText>(R.id.productprice)
+        var productqty = findViewById<EditText>(R.id.productqty)
+        var productdescription = findViewById<EditText>(R.id.productdes)
+        var submitbtn = findViewById<Button>(R.id.submitbtn)
+        var resetbtn = findViewById<Button>(R.id.resettbtn)
+
+        submitbtn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: android.view.View?) {
+                var productname = productname.getText().toString()
+                var productprice  = productprice.getText().toString()
+                var productqty = productqty.getText().toString()
+                var productdescription = productdescription.getText().toString()
+                getproduct=productname
+
+
+                if(!!productname.isEmpty() && productprice.isEmpty() && productqty.isEmpty() && productdescription.isEmpty()){}
+                else{
+                    val run = databaseref.orderByChild("productname").equalTo(productname)
+                    val valuelistener = object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            Toast.makeText(this@Add_Product,"succssfully",Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            Toast.makeText(this@Add_Product,"faild", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                }
+
+            }
+
+
+        })
+*/
     }
 }
--- app/src/main/java/com/example/pethub/MainActivity.kt
+++ app/src/main/java/com/example/pethub/MainActivity.kt
@@ -9,22 +9,22 @@ import com.google.firebase.database.FirebaseDatabase
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_view_doctors)
+        setContentView(R.layout.activity_main)
         val list: ImageButton = findViewById(R.id.listbtn)
         val profile: ImageButton = findViewById(R.id.userbtn)
         val home: ImageButton = findViewById(R.id.hometbn)
-        val addproductbtn: ImageButton = findViewById(R.id.imageButton)
+       // val addproductbtn: ImageButton = findViewById(R.id.imageButton)
         val productbtn : ImageButton = findViewById(R.id.imageproduct)
-        setContentView(R.layout.activity_view_doctors)
+        //setContentView(R.layout.activity_view_doctors)
 
         startActivity(Intent(this, ViewDoctors::class.java))
 
-        list.setOnClickListener {
+       list.setOnClickListener {
 
-            startActivity(Intent(this@MainActivity, Active_Listing::class.java))
+          startActivity(Intent(this@MainActivity, Active_Listing::class.java))
         }
         profile.setOnClickListener{
-            startActivity(Intent(this@MainActivity, MainActivity::class.java))
+            startActivity(Intent(this@MainActivity, Notification::class.java))
         }
         home.setOnClickListener{
             startActivity(Intent(this@MainActivity, MainActivity::class.java))

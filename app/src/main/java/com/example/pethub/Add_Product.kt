0bdfd3f574ca63b41c780bd38810b51371082529
Lahiru-cyber <64624843+Lahiru-cyber@users.noreply.github.com>
2022-02-28 21:35:56 +0530

    updated add product, Notification, Active Listing, myProfile

--- app/src/main/java/com/example/pethub/Add_Product.kt
+++ app/src/main/java/com/example/pethub/Add_Product.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import com.google.firebase.database.DatabaseReference
 
 class Add_Product : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)

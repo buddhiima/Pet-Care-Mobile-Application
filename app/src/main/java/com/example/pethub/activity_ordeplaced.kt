5641d7adb4115d261da5d64a5a2ad0c79dc48edd
raushan_rashid <72301703+Raushan2001@users.noreply.github.com>
2022-03-09 14:16:35 +0530

    updates

--- app/src/main/java/com/example/pethub/activity_ordeplaced.kt
+++ app/src/main/java/com/example/pethub/activity_ordeplaced.kt
@@ -1,11 +1,27 @@
 package com.example.pethub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class activity_ordeplaced : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ordeplaced)
+
+        val profile: ImageButton =findViewById(R.id.profile)
+        profile.setOnClickListener {
+            startActivity(Intent(this,BuyerProf::class.java))
+        }
+
+        val caegorie: ImageButton =findViewById(R.id.catogorie)
+        caegorie.setOnClickListener {
+            startActivity(Intent(this,Category::class.java))
+        }
+        val homebtn: ImageButton =findViewById(R.id.homebtn)
+        homebtn.setOnClickListener {
+            startActivity(Intent(this,BuyerHome::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/pethub/activity_paymentsuccess.kt
+++ app/src/main/java/com/example/pethub/activity_paymentsuccess.kt
@@ -10,10 +10,24 @@ class activity_paymentsuccess : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paymentsuccess)
 
-        val closebtn:ImageButton=findViewById(R.id.closebtn)
+        val closebtn: ImageButton = findViewById(R.id.closebtn)
 
         closebtn.setOnClickListener {
             startActivity(Intent(this@activity_paymentsuccess, BuyerHome::class.java))
+        }
+
+        val profile: ImageButton = findViewById(R.id.profile)
+        profile.setOnClickListener {
+            startActivity(Intent(this, BuyerProf::class.java))
+        }
+
+        val caegorie: ImageButton = findViewById(R.id.catogorie)
+        caegorie.setOnClickListener {
+            startActivity(Intent(this, Category::class.java))
+        }
+        val homebtn: ImageButton = findViewById(R.id.homebtn)
+        homebtn.setOnClickListener {
+            startActivity(Intent(this, BuyerHome::class.java))
         }
     }
 }

6b5c3ddef4376e79f76f55c5125d0b7d2f38749d
raushan_rashid <72301703+Raushan2001@users.noreply.github.com>
2022-03-10 13:36:44 +0530

    commit

---
+++ app/src/main/java/com/example/pethub/Vet_clinic_list.kt
@@ -0,0 +1,11 @@
+package com.example.pethub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Vet_clinic_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vet_clinic_list)
+    }
+}
